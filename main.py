0cd16de4ba5d1dfdd606820dec1e040fe761a053
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-09 04:12:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import cv2
 import tempfile
-import subprocess
+import shutil
+import numpy as np
 import uvicorn
 
 app = FastAPI()
 
-# Função para sobrepor os vídeos e manter o áudio
+# Função para sobrepor os vídeos (sem mexer no áudio)
 def overlay_videos(video_base_path, video_overlay_path, output_path):
     # Carregar os dois vídeos usando OpenCV
     cap_base = cv2.VideoCapture(video_base_path)
     cap_overlay = cv2.VideoCapture(video_overlay_path)
-    
+
     # Obter informações dos vídeos
     fps = cap_base.get(cv2.CAP_PROP_FPS)
     frame_width = int(cap_base.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -22,7 +23,7 @@ def overlay_videos(video_base_path, video_overlay_path, output_path):
     # Criar o writer para salvar o vídeo final
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Usando o codec mp4v
     out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
-    
+
     while cap_base.isOpened() and cap_overlay.isOpened():
         ret_base, frame_base = cap_base.read()
         ret_overlay, frame_overlay = cap_overlay.read()
@@ -38,20 +39,12 @@ def overlay_videos(video_base_path, video_overlay_path, output_path):
 
         # Escreve o frame no arquivo de saída
         out.write(frame_overlay_resized)
-    
+
     # Libera os recursos
     cap_base.release()
     cap_overlay.release()
     out.release()
-
     print(f"Vídeo final gerado em: {output_path}")
-
-# Função para adicionar o áudio ao vídeo
-def add_audio_to_video(video_path, audio_path, output_path):
-    cmd = [
-        'ffmpeg', '-i', video_path, '-i', audio_path, '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', '-y', output_path
-    ]
-    subprocess.run(cmd)
 
 # Endereço da API
 @app.post("/overlay/")
@@ -73,9 +66,6 @@ async def overlay_api(video_base: UploadFile = File(...), video_overlay: UploadF
     try:
         # Chama a função para sobrepor os vídeos
         overlay_videos(temp_video_base, temp_video_overlay, temp_output_video)
-
-        # Mantém o áudio original do vídeo base
-        add_audio_to_video(temp_output_video, temp_video_base, temp_output_video)
 
         # Envia o arquivo gerado como resposta
         return FileResponse(temp_output_video, media_type='video/mp4', filename='nome.mp4')
