e7ea45fa8965b23d98cae1ac3f986026e14b5d66
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-09 21:38:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import uvicorn
 
 app = FastAPI()
 
-def overlay_videos_with_audio(video_base_path, video_overlay_path, output_path):
+def overlay_videos_with_audio(video_base_path, video_overlay_path, output_path, transparencia):
     # Carregar os clipes de vídeo usando o MoviePy
     base_clip = VideoFileClip(video_base_path)
     overlay_clip = VideoFileClip(video_overlay_path)
@@ -16,7 +16,7 @@ def overlay_videos_with_audio(video_base_path, video_overlay_path, output_path):
     overlay_resized = overlay_clip.resize(base_clip.size)
     
     # Combinar os vídeos com transparência (caso necessário)
-    video_combined = CompositeVideoClip([base_clip, overlay_resized.set_opacity(0.15)])
+    video_combined = CompositeVideoClip([base_clip, overlay_resized.set_opacity(transparencia)])
     
     # Caso tenha áudio no vídeo base, podemos adicionar ao vídeo combinado
     if base_clip.audio:
@@ -34,7 +34,7 @@ def overlay_videos_with_audio(video_base_path, video_overlay_path, output_path):
     video_combined.write_videofile(output_path, codec="libx264", audio_codec="aac")
 
 @app.post("/overlay/")
-async def overlay_api(video_base: UploadFile = File(...), video_overlay: UploadFile = File(...)):
+async def overlay_api(video_base: UploadFile = File(...), video_overlay: UploadFile = File(...), transparencia: float = 0.05):
     temp_video_base = tempfile.mktemp(suffix='.mp4')
     temp_video_overlay = tempfile.mktemp(suffix='.mp4')
     temp_output_video = tempfile.mktemp(suffix='.mp4')
@@ -45,7 +45,7 @@ async def overlay_api(video_base: UploadFile = File(...), video_overlay: UploadF
         f.write(await video_overlay.read())
     
     try:
-        overlay_videos_with_audio(temp_video_base, temp_video_overlay, temp_output_video)
+        overlay_videos_with_audio(temp_video_base, temp_video_overlay, temp_output_video, transparencia)
         return FileResponse(temp_output_video, media_type='video/mp4', filename='output.mp4')
     except Exception as e:
         return {"error": str(e)}
