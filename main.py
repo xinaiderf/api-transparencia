4e23297aaa97a45c5bf6e43129ae56de62b6f317
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-11 15:31:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def overlay_videos_with_audio(video_base_path, video_overlay_path, output_path, 
         output_path,
         codec="libx264",
         audio_codec="aac",
-        threads=12,              # utiliza múltiplas threads conforme a capacidade da máquina
+        threads=4,              # utiliza múltiplas threads conforme a capacidade da máquina
         preset="ultrafast",      # acelera a codificação (pode aumentar o tamanho do arquivo)
         ffmpeg_params=["-crf", "28"],  # ajusta a qualidade para reduzir o tempo de processamento
         logger=None              # desativa logs detalhados para diminuir overhead
